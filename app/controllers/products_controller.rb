2f82e530020d94ee4ed87b87125d5aec579b7c74
Adam89 <adam.dev89@gmail.com>
2015-01-17 20:32:34 +0000

    Created initial structure of store with products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,54 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Product.find(params[:id]).destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :image,
+      :price,
+    )
+  end
+end
